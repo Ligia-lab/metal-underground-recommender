a4be47c610ea90c03668f8d718d8d0bed7fe8836
Ligia <ligiacamara92@gmail.com>
2025-12-12 11:10:41 -0300

    fix parte do código de teste foi comentado

--- notebooks/testes.py
+++ notebooks/testes.py
@@ -15,89 +15,65 @@ from src.dataset import expand_artists_from_user_likes
 
 #%%
 
-sp = get_spotify_client()
+# sp = get_spotify_client()
 
-#%%
+# results = sp.search(q='Mastodon', type='artist', limit=1)
+# results['artists']['items'][0]['name'], results['artists']['items'][0]['id']
 
-results = sp.search(q='Mastodon', type='artist', limit=1)
-results['artists']['items'][0]['name'], results['artists']['items'][0]['id']
+# artist = get_artist_by_name(sp, "Mastodon")
 
-# %%
+# if artist is None:
+#     print("Não encontrou o artista.")
+# else:
+#     print("Nome :", artist["name"])
+#     print("ID   :", artist["id"])
+#     print("Pop  :", artist["popularity"])
+#     print("Genres:", artist["genres"])
 
-artist = get_artist_by_name(sp, "Mastodon")
+# seed_artists_names = [
+#     'Mastodon',
+#     'Gojira',
+#     'Jinjer',
+#     'Tool',
+#     'Lacuna Coil',
+#     'Eths',
+#     'Alien Weaponry',
+#     'Crypta',
+#     'Epica',
+#     'Ghost',
+#     'HIM',
+#     'Dir en Grey', 
+#     'Nightwish',
+#     'Type O Negative'   
+# ]
 
-if artist is None:
-    print("Não encontrou o artista.")
-else:
-    print("Nome :", artist["name"])
-    print("ID   :", artist["id"])
-    print("Pop  :", artist["popularity"])
-    print("Genres:", artist["genres"])
+# all_artists = collect_seed_artists(sp, seed_artists_names)
 
-# %%
+# len(all_artists)
 
-seed_artists_names = [
-    'Mastodon',
-    'Gojira',
-    'Jinjer',
-    'Tool',
-    'Lacuna Coil',
-    'Eths',
-    'Alien Weaponry',
-    'Crypta',
-    'Epica',
-    'Ghost',
-    'HIM',
-    'Dir en Grey', 
-    'Nightwish',
-    'Type O Negative'   
-]
+# df_artists = build_basic_artists_df(all_artists)
+# df_artists
 
-#%%
+# df_artists.to_csv("../data/artists_basic.csv", index=False)
 
-all_artists = collect_seed_artists(sp, seed_artists_names)
+# df_artists = pd.read_csv('../data/artists_basic.csv')
 
-#%%
+# df_artists.head()
 
-len(all_artists)
+# df_with_genres, mlb = add_genre_vectors(df_artists)
+# df_with_genres.head()
 
-# %%
+# X, genre_cols = get_genre_feature_matrix(df_with_genres)
+# X.shape, len(genre_cols)
 
-df_artists = build_basic_artists_df(all_artists)
-df_artists
+# recs = recommend_artists_by_genre(
+#     df_with_genres,
+#     user_likes=["Gojira", "Meshuggah"],
+#     top_k=10,
+#     underground_weight=0.3,
+# )
 
-# %%
-
-df_artists.to_csv("../data/artists_basic.csv", index=False)
-
-# %%
-
-df_artists = pd.read_csv('../data/artists_basic.csv')
-
-# %%
-
-df_artists.head()
-
-#%%
-
-df_with_genres, mlb = add_genre_vectors(df_artists)
-df_with_genres.head()
-
-# %%
-
-X, genre_cols = get_genre_feature_matrix(df_with_genres)
-X.shape, len(genre_cols)
-
-# %%
-
-recs = recommend_artists_by_genre(
-    df_with_genres,
-    user_likes=["Gojira", "Meshuggah"],
-    top_k=10,
-    underground_weight=0.3,
-)
-
-recs[["name", "genres", "popularity", "similarity", "underground_score", "final_score"]]
+# recs[["name", "genres", "popularity", "similarity", "underground_score", "final_score"]]
 
 # %%
 
@@ -119,7 +95,6 @@ user_likes = [
     'Type O Negative'    
 ]
 
-
 df_with_genres = expand_artists_from_user_likes(sp, user_likes)
 
 recs = recommend_artists_by_genre(
@@ -131,7 +106,7 @@ recs = recommend_artists_by_genre(
 
 recs[["name", "genres", "popularity", "similarity", "underground_score", "final_score"]]
 
-# %%
+#%%
 
 len(df_with_genres)
 
